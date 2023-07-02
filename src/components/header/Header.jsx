09873f17584d03f5eca67d09d959ec642a05e636
Dev-De <chaudharydeepak47667@gmail.com>
2023-07-02 16:43:55 +0530

    Final Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoGo from "../../assets/react.svg";
+import "./Header.css";
+const Header = () => {
+	return (
+		<header>
+			<h1>🛒 Ecommerce App</h1>
+		</header>
+	);
+};
+
+export default Header;
